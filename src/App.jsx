7bd7bdbc1d8784c4cc77d2ee0901b67c163b03b0
Arch <wbhaoui@gmail.com>
2024-11-07 22:44:21 +0100

    Implement solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
-
-
+import { useState } from "react";
+import "reset-css";
+import "./stylesheets/App.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import TodoHeader from "./components/TodoHeader";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return ''
+  const [count, setCount] = useState(0);
+  const defaultTodos = [
+    { task: "h", done: false, id: 0 },
+    { task: "b", done: true, id: 1 },
+    { task: "c", done: true, id: 2 },
+  ];
+  const [todos, setTodos] = useState(defaultTodos);
+  return (
+    <main>
+      <TodoHeader />
+      <TodoInput todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TodoHeader.jsx
@@ -0,0 +1,19 @@
+import "../stylesheets/TodoHeader.css"
+
+const TodoHeader = () => {
+  return (
+    <h1>
+      <span>T</span>
+      <span>o</span>
+      <span>d</span>
+      <span>o</span>
+      <span> </span>
+      <span>L</span>
+      <span>i</span>
+      <span>s</span>
+      <span>t</span>
+    </h1>
+  );
+};
+
+export default TodoHeader;
