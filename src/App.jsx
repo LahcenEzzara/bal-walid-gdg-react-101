082876a12d0b1311dfebf365404b2304c53080de
bal-wal <balhaoui.walid@ensam-casa.ma>
2024-11-08 12:07:40 +0100

    Move source code to /solution folder, create new empty files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import { useState } from "react";
 import "reset-css";
-import "./stylesheets/App.css";
-import TodoInput from "./components/TodoInput";
-import TodoList from "./components/TodoList";
+
 import TodoHeader from "./components/TodoHeader";
+
 function App() {
-  const defaultTodos = [
-    { task: "Tyb lftor", done: true, id: 0 },
-    { task: "Walk to school", done: true, id: 1 },
-    { task: "Finish react workshop", done: false, id: 2 },
-  ];
-  const [todos, setTodos] = useState(defaultTodos);
+
+
   return (
     <main>
       <TodoHeader />
-      <TodoInput todos={todos} setTodos={setTodos} />
-      <TodoList todos={todos} setTodos={setTodos} />
     </main>
   );
 }
