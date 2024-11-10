eb4a22f65bfaeb48440b08c1bcae25a16d776512
Arch <wbhaoui@gmail.com>
2024-11-10 12:01:49 +0100

    Recommit solution

---
+++ solution/components/TodoList.jsx
@@ -0,0 +1,27 @@
+// import "../stylesheets/TodoList.css"
+
+const TodoList = ({ todos, setTodos }) => {
+  const toggleTodo = (id) => {
+    setTodos(todos.map(
+      (todo) => {
+        if (todo.id != id) {
+          return todo;
+        } else {
+          return {...todo, done: !todo.done}
+        }
+      }
+    ))
+  }
+  return (
+    <ul>
+      {todos.map((todo, index) => 
+        <li key={index}>
+          <input onChange={() => toggleTodo(todo.id)} type={"checkbox"} checked={todo.done} />
+          {todo.task}
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default TodoList;
